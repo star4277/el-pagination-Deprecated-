a2cbe9e6bb4b6a5d3ae791bee6cc6513b433334f
始梦少年 <chibeiyu9907@163.com>
2025-07-20 08:15:21 +0800

    添加属性

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import {ElPagination} from "element-plus";
+import {ref} from "vue";
+const data = ref({
+  current: 1,
+  size: 10,
+  total: 50
+})
 </script>
 
 <template>
- <el-pagination/>
+ <el-pagination :total="data.total" :current-page="data.current" :page-size="data.size"/>
 </template>
 
 <style scoped>
